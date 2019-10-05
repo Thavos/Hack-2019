fc35c6ba307131c728ea1f5a8990358c1a4f1128
THAVOS\Simon <simon.hruz252@gmail.com>
2019-10-05 20:04:26 +0200

    Kose

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000;
 const axios = require('axios');
+const transformation = require('transform-coordinates')
 
+let GeoJson;
 let VyznamneObjekty;
+
+const Kose = [];
 
 const getData = async () => {
   const {data} = await axios.get('https://egov.presov.sk/Default.aspx?NavigationState=778:0::plac1889:_144153_5_8');
@@ -10,10 +14,31 @@ const getData = async () => {
   return data;
 }
 
+const getGeoData = async () => {
+
+  const {data} = await axios.get('https://egov.presov.sk/GeoDataKatalog/separovany_zber.json');
+
+  let geoData = data.features;
+  let newGeoData = [];
+
+  const transform = transformation('5514', 'EPSG:4326');
+  geoData.forEach((element, index) => {
+    let myX = element.geometry.coordinates[0].join(',').split(',')[0];
+    let myY = element.geometry.coordinates[0].join(',').split(',')[1];
+    newGeoData.push(transform.forward({x: parseFloat(myX), y: parseFloat(myY)}));
+  });
+
+  geoData.forEach((element, index) => {
+    Kose.push({adress : element.properties['N_OBJ'], coordinates : {longitude : newGeoData[index].y, latitude : newGeoData[index].x}})
+  });
+
+  return Kose;
+}
+
 
 express()
-  .get('/', (req, res) => {
-    getData().then((data) => {
+  .get('/kose', (req, res) => {
+    getGeoData().then((data) => {
       res.send(data);  
     })
   })
