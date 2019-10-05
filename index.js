dd0af5a9aec2274f2bf824313741cc82b478e200
THAVOS\Simon <simon.hruz252@gmail.com>
2019-10-05 12:44:41 +0200

    data fix

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000;
 
-
-////////////////////DATASETS//////
-
-var fs = require('fs');
-
-function readJSONFile(filename, callback) {
-  fs.readFile(filename, function (err, data) {
-    if(err) {
-      callback(err);
-      return;
-    }
-    try {
-      callback(null, JSON.parse(data));
-    } catch(exception) {
-      callback(exception);
-    }
-  });
-}
-
-///////////////////////////////
-
 express()
   .get('/', (req, res) => res.send('Home Page'))
-  .get('/get', function(req, res){
-    readJSONFile('https://egov.presov.sk/Default.aspx?NavigationState=778:0::plac1889:_144153_5_8', function (err, json) {
-      if(err) { throw err; }
-      console.log(json);
-    });
-    res.send(json);
-  })
   .post('/post', (req, res => res.send('post')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
