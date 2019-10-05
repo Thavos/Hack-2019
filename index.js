9675606f56ca10f6a83c5c88f71ff7a896d92759
THAVOS\Simon <simon.hruz252@gmail.com>
2019-10-05 16:46:03 +0200

    fix

--- index.js
+++ index.js
@@ -7,18 +7,8 @@ let VyznamneObjekty;
 const getData = async () => {
   const {data} = await axios.get('https://egov.presov.sk/Default.aspx?NavigationState=778:0::plac1889:_144153_5_8');
   VyznamneObjekty = data;
-  //filter();
   return data;
 }
-
-/*function filter(){
-  VyznamneObjekty.forEach(function(element) {
-    if(element["Skupina"] == 'Fontány a pramene'){
-      objekty += element + ',';
-    }
-  });
-  console.log(objekty);
-}*/
 
 
 express()
@@ -28,3 +18,13 @@ express()
     })
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+
+
+
+
+
+
+
+
