a12786e3db087bc99b110d42241f54884b8578dc
THAVOS\Simon <simon.hruz252@gmail.com>
2019-10-05 14:03:56 +0200

    fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 const axios = require('axios');
 
-let = VyznamneObjekty;
+let VyznamneObjekty;
  
 (async () => {
   const response = await axios.get('https://egov.presov.sk/Default.aspx?NavigationState=778:0::plac1889:_144153_5_8')
