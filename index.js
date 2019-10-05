af14fe24975269160a8a6cd8fc8f6ea10c5e3206
THAVOS\Simon <simon.hruz252@gmail.com>
2019-10-05 11:49:57 +0200

    app 1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const PORT = process.env.PORT || 5000;
 
 express()
-  .get('/', (req, res) => res.send('Hello World'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => res.send('Home Page'))
+  .get('/get', (req, res) => res.send('get'))
+  .post('/post', (req, res => res.secure('post')))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
