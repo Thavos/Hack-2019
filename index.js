9903d1f683a30212f0b66b8294961f5163778ec4
THAVOS\Simon <simon.hruz252@gmail.com>
2019-10-05 21:24:33 +0200

    fix

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const transformation = require('transform-coordinates')
 let GeoJson;
 let VyznamneObjekty;
 
-const Kose = [];
+const Data = [];
+
 
 const getData = async () => {
   const {data} = await axios.get('https://egov.presov.sk/Default.aspx?NavigationState=778:0::plac1889:_144153_5_8');
@@ -14,12 +15,13 @@ const getData = async () => {
   return data;
 }
 
-const getGeoData = async () => {
+const getGeoData1 = async (url) => {
 
-  const {data} = await axios.get('https://egov.presov.sk/GeoDataKatalog/separovany_zber.json');
+  const {data} = await axios.get(url);
 
   let geoData = data.features;
   let newGeoData = [];
+  console.log(geoData[0].geometry.coordinates.join(',').split(','));
 
   const transform = transformation('5514', 'EPSG:4326');
   geoData.forEach((element, index) => {
@@ -29,16 +31,56 @@ const getGeoData = async () => {
   });
 
   geoData.forEach((element, index) => {
-    Kose.push({adress : element.properties['N_OBJ'], coordinates : {longitude : newGeoData[index].y, latitude : newGeoData[index].x}})
+    Data.push({adress : element.properties['N_OBJ'], coordinates : {longitude : newGeoData[index].y, latitude : newGeoData[index].x}})
   });
 
-  return Kose;
+  return Data;
 }
 
+const getGeoData2 = async (url) => {
+
+  const {data} = await axios.get(url);
+
+  let geoData = data.features;
+  let newGeoData = [];
+
+  const transform = transformation('5514', 'EPSG:4326');
+  geoData.forEach((element, index) => {
+    let myX = element.geometry.coordinates.join(',').split(',');
+    let myY = element.geometry.coordinates.join(',').split(',');
+    newGeoData.push(transform.forward({x: parseFloat(myX), y: parseFloat(myY)}));
+  });
+
+  geoData.forEach((element, index) => {
+    Data.push({adress : element.properties['N_OBJ'], coordinates : {longitude : newGeoData[index].y, latitude : newGeoData[index].x}})
+  });
+
+  return Data;
+}
 
 express()
   .get('/kose', (req, res) => {
-    getGeoData().then((data) => {
+    getGeoData1('https://egov.presov.sk/GeoDataKatalog/separovany_zber.json').then((data) => {
+      res.send(data);  
+    })
+  })
+  .get('/kontajnery', (req, res) => {
+    getGeoData1('https://egov.presov.sk/GeoDataKatalog/velkoobjemove_kontajnery.json').then((data) => {
+      res.send(data);  
+    })
+  })
+  .get('/pamiatky', (req, res) => {
+    getGeoData2('https://egov.presov.sk/GeoDataKatalog/sakralne_objekty.json').then((data) => {
+      res.send(data);  
+    })
+  })
+  .get('/fontany', (req, res) => {
+    getGeoData2('https://egov.presov.sk/GeoDataKatalog/fontany_pramene.json').then((data) => {
+      res.send(data);  
+    })
+  })
+  .get('/wc', (req, res) => {
+    getGeoData2('https://egov.presov.sk/GeoDataKatalog/verejne_wc.json').then((data) => {
       res.send(data);  
     })
   })
