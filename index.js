12d2bfd0998d6f38b26e543deb739407dfb4b303
THAVOS\Simon <simon.hruz252@gmail.com>
2019-10-05 14:53:52 +0200

    3

--- index.js
+++ index.js
@@ -3,15 +3,10 @@ const PORT = process.env.PORT || 5000;
 const axios = require('axios');
 
 let VyznamneObjekty;
- 
-(async () => {
-  const response = await axios.get('https://egov.presov.sk/Default.aspx?NavigationState=778:0::plac1889:_144153_5_8');
-  VyznamneObjekty = response;
-  console.log(VyznamneObjekty);
-})()
 
 const getData = async () => {
   const {data} = await axios.get('https://egov.presov.sk/Default.aspx?NavigationState=778:0::plac1889:_144153_5_8');
+  VyznamneObjekty = data;
   return data;
 }
 
@@ -22,5 +17,4 @@ express()
       res.send(data[1]);  
     })
   })
- //.get('/Objekty', (req, res) => res.send(VyznamneObjekty))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
