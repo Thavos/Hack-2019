19b5a94633a68221a5e150b42e0d4c765e15f6c4
THAVOS\Simon <simon.hruz252@gmail.com>
2019-10-05 13:03:15 +0200

    fix

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ const PORT = process.env.PORT || 5000;
 
 express()
   .get('/', (req, res) => res.send('Home Page'))
-  .post('/post', (req, res => res.send('post')))
+  .post('/post', function(){
+    let data = "https://egov.presov.sk/Default.aspx?NavigationState=778:0::plac1889:_144153_5_8";
+    JSON.parse(data);
+    res.send(data);
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+  
