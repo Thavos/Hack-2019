ff9896d02543a0d44d084cd6b01eeeb1dc0a43c0
THAVOS\Simon <simon.hruz252@gmail.com>
2019-10-05 21:50:12 +0200

    fixed

--- index.js
+++ index.js
@@ -3,21 +3,14 @@ const PORT = process.env.PORT || 5000;
 const axios = require('axios');
 const transformation = require('transform-coordinates')
 
-let GeoJson;
-let VyznamneObjekty;
+let Kose = [];
+let Kontajnery = [];
+let Fontany = [];
+let Pamiatky = [];
+let Wc = [];
 
-const Data = [];
-const Data2 = [];
-
-
-const getData = async () => {
-  const {data} = await axios.get('https://egov.presov.sk/Default.aspx?NavigationState=778:0::plac1889:_144153_5_8');
-  VyznamneObjekty = data;
-  return data;
-}
-
-const getGeoData1 = async (url) => {
-
+const KoseF = async (url) => {
+  Kose = [];
   const {data} = await axios.get(url);
 
   let geoData = data.features;
@@ -32,18 +25,41 @@ const getGeoData1 = async (url) => {
   });
 
   geoData.forEach((element, index) => {
-    Data.push({adress : element.properties['N_OBJ'], coordinates : {longitude : newGeoData[index].y, latitude : newGeoData[index].x}})
+    Kose.push({adress : element.properties['N_OBJ'], coordinates : {longitude : newGeoData[index].y, latitude : newGeoData[index].x}})
   });
 
-  return Data;
+  return Kose;
 }
 
-const getGeoData2 = async (url) => {
-
+const KontajneryF = async (url) => {
+  Kontajnery = [];
   const {data} = await axios.get(url);
 
   let geoData = data.features;
   let newGeoData = [];
+  console.log(geoData[0].geometry.coordinates.join(',').split(','));
+
+  const transform = transformation('5514', 'EPSG:4326');
+  geoData.forEach((element, index) => {
+    let myX = element.geometry.coordinates[0].join(',').split(',')[0];
+    let myY = element.geometry.coordinates[0].join(',').split(',')[1];
+    newGeoData.push(transform.forward({x: parseFloat(myX), y: parseFloat(myY)}));
+  });
+
+  geoData.forEach((element, index) => {
+    Kontajnery.push({adress : element.properties['N_OBJ'], coordinates : {longitude : newGeoData[index].y, latitude : newGeoData[index].x}})
+  });
+
+  return Kontajnery;
+}
+
+const PamiatkyF = async (url) => {
+  Pamiatky = [];
+  const {data} = await axios.get(url);
+
+  let geoData = data.features;
+  let newGeoData = [];
+  console.log(geoData[0].geometry.coordinates.join(',').split(','));
 
   const transform = transformation('5514', 'EPSG:4326');
   geoData.forEach((element, index) => {
@@ -53,35 +69,79 @@ const getGeoData2 = async (url) => {
   });
 
   geoData.forEach((element, index) => {
-    Data2.push({adress : element.properties['N_OBJ'], coordinates : {longitude : newGeoData[index].y, latitude : newGeoData[index].x}})
+    Pamiatky.push({adress : element.properties['N_OBJ'], coordinates : {longitude : newGeoData[index].y, latitude : newGeoData[index].x}})
   });
 
-  return Data2;
+  return Pamiatky;
+}
+
+const FontanyF = async (url) => {
+  Fontany = [];
+  const {data} = await axios.get(url);
+
+  let geoData = data.features;
+  let newGeoData = [];
+  console.log(geoData[0].geometry.coordinates.join(',').split(','));
+
+  const transform = transformation('5514', 'EPSG:4326');
+  geoData.forEach((element, index) => {
+    let myX = element.geometry.coordinates.join(',').split(',');
+    let myY = element.geometry.coordinates.join(',').split(',');
+    newGeoData.push(transform.forward({x: parseFloat(myX), y: parseFloat(myY)}));
+  });
+
+  geoData.forEach((element, index) => {
+    Fontany.push({adress : element.properties['N_OBJ'], coordinates : {longitude : newGeoData[index].y, latitude : newGeoData[index].x}})
+  });
+
+  return Fontany;
+}
+
+const WcF = async (url) => {
+  Wc = [];
+  const {data} = await axios.get(url);
+
+  let geoData = data.features;
+  let newGeoData = [];
+  console.log(geoData[0].geometry.coordinates.join(',').split(','));
+
+  const transform = transformation('5514', 'EPSG:4326');
+  geoData.forEach((element, index) => {
+    let myX = element.geometry.coordinates.join(',').split(',');
+    let myY = element.geometry.coordinates.join(',').split(',');
+    newGeoData.push(transform.forward({x: parseFloat(myX), y: parseFloat(myY)}));
+  });
+
+  geoData.forEach((element, index) => {
+    Wc.push({adress : element.properties['N_OBJ'], coordinates : {longitude : newGeoData[index].y, latitude : newGeoData[index].x}})
+  });
+
+  return Wc;
 }
 
 express()
   .get('/kose', (req, res) => {
-    getGeoData1('https://egov.presov.sk/GeoDataKatalog/separovany_zber.json').then((data) => {
-      res.send(data);  
+    KoseF('https://egov.presov.sk/GeoDataKatalog/separovany_zber.json').then((data) => {
+      res.send(data);
     })
   })
   .get('/kontajnery', (req, res) => {
-    getGeoData1('https://egov.presov.sk/GeoDataKatalog/velkoobjemove_kontajnery.json').then((data) => {
+    KontajneryF('https://egov.presov.sk/GeoDataKatalog/velkoobjemove_kontajnery.json').then((data) => {
       res.send(data);  
     })
   })
   .get('/pamiatky', (req, res) => {
-    getGeoData2('https://egov.presov.sk/GeoDataKatalog/sakralne_objekty.json').then((data) => {
+    PamiatkyF('https://egov.presov.sk/GeoDataKatalog/sakralne_objekty.json').then((data) => {
       res.send(data);  
     })
   })
   .get('/fontany', (req, res) => {
-    getGeoData2('https://egov.presov.sk/GeoDataKatalog/fontany_pramene.json').then((data) => {
+    FontanyF('https://egov.presov.sk/GeoDataKatalog/fontany_pramene.json').then((data) => {
       res.send(data);  
     })
   })
   .get('/wc', (req, res) => {
-    getGeoData2('https://egov.presov.sk/GeoDataKatalog/verejne_wc.json').then((data) => {
+    WcF('https://egov.presov.sk/GeoDataKatalog/verejne_wc.json').then((data) => {
       res.send(data);  
     })
   })
