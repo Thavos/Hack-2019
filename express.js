6df0685dde0f700e72734dc0b6841dc23356aa35
THAVOS\Simon <simon.hruz252@gmail.com>
2019-10-05 11:25:59 +0200

    app fix

--- express.js
+++ express.js
@@ -1,6 +1,6 @@
 const express = require('express', '4.17.1')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
