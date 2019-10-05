ffce7f708453341ef502447ec6a85c096be85857
THAVOS\Simon <simon.hruz252@gmail.com>
2019-10-05 12:30:08 +0200

    app 2

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ express()
   .get('/get', (req, res) => res.send('get'))
   .post('/post', (req, res => res.secure('post')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+////////////////////DATASETS
+
+  var fs = require('fs');
+
+  function readJSONFile(filename, callback) {
+    fs.readFile(filename, function (err, data) {
+      if(err) {
+        callback(err);
+        return;
+      }
+      try {
+        callback(null, JSON.parse(data));
+      } catch(exception) {
+        callback(exception);
+      }
+    });
+  }
+
+  readJSONFile('../../data.json', function (err, json) {
+    if(err) { throw err; }
+    console.log(json);
+  });
