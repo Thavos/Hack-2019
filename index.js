05b5d1c10a2e48a2854a4c1b8ee60e86e8eba940
THAVOS\Simon <simon.hruz252@gmail.com>
2019-10-05 15:53:42 +0200

    testing

--- index.js
+++ index.js
@@ -7,14 +7,24 @@ let VyznamneObjekty;
 const getData = async () => {
   const {data} = await axios.get('https://egov.presov.sk/Default.aspx?NavigationState=778:0::plac1889:_144153_5_8');
   VyznamneObjekty = data;
+  //filter();
   return data;
 }
+
+/*function filter(){
+  VyznamneObjekty.forEach(function(element) {
+    if(element["Skupina"] == 'Fontány a pramene'){
+      objekty += element + ',';
+    }
+  });
+  console.log(objekty);
+}*/
 
 
 express()
   .get('/', (req, res) => {
     getData().then((data) => {
-      res.send(data[1]);  
+      res.send(data);  
     })
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
